5b80a7ad2af6951cd2057c5d1a28c0b53c3bac39
Isaac Plunkett <isaacp@email.arizona.edu>
2017-10-22 13:43:08 -0700

    added a few more unit tests

--- OpenBurn/src/tests/UnitConverterTest.java
+++ OpenBurn/src/tests/UnitConverterTest.java
@@ -12,4 +12,11 @@ public class UnitConverterTest
 		assertEquals(0.0, UnitConverter.convertLenghtMtoI(0), 0.0000000000000000001);
 		assertEquals(1.0, UnitConverter.convertLenghtMtoI(2.54), 0.0000000000000000001);
 	}
+	
+	@Test
+	public void testConvertLenghtItoM()
+	{
+		assertEquals(0.0, UnitConverter.convertLenghtItoM(0), 0.0000000000000000001);
+		assertEquals(2.54, UnitConverter.convertLenghtItoM(1.0), 0.0000000000000000001);
+	}
 }
