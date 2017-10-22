dd48765e4667e36d9dbe26bd2037e396bc2b8124
arane <arane@dhcp-10-132-171-164.uawifi.arizona.edu>
2017-10-22 13:28:00 -0700

    InputClassifier added for real this time

---
+++ OpenBurn/src/model/ImpulseClassifier.java
@@ -0,0 +1,16 @@
+package model;
+
+public class ImpulseClassifier {
+	private String classification;
+	private double ISP;
+	
+	
+	public String getClassification(){
+		return classification;
+	}
+	
+	public double ISP(){
+		return ISP;
+	}
+	
+}
