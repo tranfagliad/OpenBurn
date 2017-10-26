19f3893acebdcb41ea1c4c94219608e4ebeaa70a
Vicente Etitien Figueroa <vicenteettienf@gmail.com>
2017-10-26 11:50:48 -0600

    added some variables

--- OpenBurn/src/view/RSEGenerator.java
+++ OpenBurn/src/view/RSEGenerator.java
@@ -22,6 +22,12 @@ public class RSEGenerator
 	public RSEGenerator( String teamName, SimulationResults results, Case c, ImpulseClassifier classifier)
 	{
 		// list simresults object: mass, thrust, cg, time; nozzel object,case object: casemass, diameter, length, Classiferier: clasification, isp, mass_frac. string team name	
+		String classification = classifier.getClassification(); 
+		double isp = classifier.ISP();
+		double mass_frac = classifier.getMassFrac();
+		double diameter = c.getDiameter();
+		double [] mass = results.getMassFlowPerAreaGrain();
+		double time = results.getTime();
 		/*
 		rse_file_name = sprintf("%s.rse",classification);
 		rse_file_name = strrep(rse_file_name," ","_");
