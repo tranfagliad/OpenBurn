eebbde1feb210a07970d2b428a759871f36654bf
Vicente Etitien Figueroa <vicenteettienf@gmail.com>
2017-11-11 14:04:41 -0600

    Made the proper adjustments to the output of the file to match the rocksim file

--- OpenBurn/src/view/RSEGenerator.java
+++ OpenBurn/src/view/RSEGenerator.java
@@ -4,6 +4,7 @@ package view;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.Case;
@@ -24,77 +25,150 @@ public class RSEGenerator
 	 * 
 	 * Purpose: 
 	**/
+	private static final String DIR_ERROR_MSG = "ERROR: Results folder could not be created!\n";
+	
+	// Other strings
+	private static final String CSV_EXTENSION = ".csv";
+	private static final String RESULTS_DIRECTORY = "./../OpenBurn_results/";
+	private String classification;
+	private double isp;
+	private double massFrac;
+	private double diameter;
+	private double mass;
+	private double time;
+	private double throatDiameter;
+	private double thrust;
+	private double exitDiameter;
+	private double caseMass;
+	private double length;
+	private double impulse;
+	private double reloadable;
 	
 	public RSEGenerator( String teamName, List <SimulationResults> results, Case c, SimulationSummary classifier, Nozzle no)
 	{
-		// list simresults object: mass, thrust, cg, time; nozzel object,case object: casemass, diameter, length, Classiferier: clasification, isp, mass_frac. string team name	
-		String classification = classifier.getClassification(); 
-		double isp = classifier.ISP();
-		double massFrac = classifier.getMassFrac();
-		double diameter = c.getDiameter();
-		double  mass = results.get(0).getSystemMass();
-		double time = results.get(0).getTime();
-		double throatDiameter = no.getThroatDiameter();
-		double thrust = results.get(0).getThrust();
-		double exitDiameter = no.getExitDiameter();
-		double caseMass = c.getCaseMass();
-		double length = c.getLength();
-	//	rse_file_name = sprintf("%s.rse",classification);
-	//	rse_file_name = strrep(rse_file_name," ","_");
-	//	fid = fopen(rse_file_name,"wt");
-		File file = new File("C:/Users/EVHfa/Desktop/436workspace/GitHub/OpenBurn/OpenBurn_results/results.rse");
-		file.getParentFile().mkdirs();
+		classification ="Test";//classifier.getClassification(); 
+		isp =0;//classifier.ISP();
+		massFrac =0;//classifier.getMassFrac();
+		diameter =0; //c.getDiameter();
+		mass =0; //results.get(0).getSystemMass();
+		time =5; //results.get(0).getTime();
+		throatDiameter =0; //no.getThroatDiameter();
+		thrust =0; //results.get(0).getThrust();
+		exitDiameter =0; //no.getExitDiameter();
+		caseMass =0; //c.getCaseMass();
+		length =0; //c.getLength();
+		impulse =0; //classifier.getImpulse();
+		//reloadable =0; //0;
+		
+		File resultsDir = new File(RESULTS_DIRECTORY);
+		if (!resultsDir.exists())
+			if (resultsDir.mkdir() == false)
+				throw new RuntimeException(DIR_ERROR_MSG);
+		
+		File file = new File(RESULTS_DIRECTORY+"results.rse");
+		//file.getParentFile().mkdirs();
 
 		try {
 			PrintWriter printWriter = new PrintWriter(file);
 		
-		//%Header stuff
+		//Header stuff
 		
 		printWriter.println("<engine-database>");
 		printWriter.println("  <engine-list>");
 
-		//%Engine data
-		printWriter.print("    <engine  mfg='UAWR' ");
-		printWriter.print("code="+classification+" ");
-		//printWriter.print("Type="+reloadable+" "); //?
-		printWriter.print("dia="+diameter+" ");
-		printWriter.println("len="+length); // case length? motor_length
-		printWriter.print("initWt="+mass+" "); // mass at i?
-		printWriter.print("propWt="+(mass - caseMass)+" ");
-		printWriter.println("delays="+1000+" auto-calc-mass='1'");
-		printWriter.print("auto-calc-cg='1' ");
-		printWriter.print("avgThrust="+thrust+" "); //mean
-		printWriter.print("peakThrust="+thrust+" "); //max
-		printWriter.println("throatDia="+throatDiameter);
-		printWriter.print("exitDia="+exitDiameter+" ");
-		//printWriter.print("Itot="impulse+" "); //?
-		//printWriter.print("burn-time="+time(length(time))+" "); //?
-		printWriter.print("massFrac="+massFrac+" ");
-		printWriter.println("Isp="+isp);
-		printWriter.println("tDiv='20' tStep='-1.' tFix='1' FDiv='20' FStep='-1.' FFix='1' mDiv='10'");
-		printWriter.print("mStep='-1.' mFix='1' cgDiv='10' cgStep='-1.' cgFix='1'>");
+		//Engine data
+		printWriter.print("    <engine  mfg=\"UAWR\" ");
+		printWriter.print("code=\""+classification+"\" ");
+		printWriter.print("Type=\"reloadable\" "); // set with null value, not in own file yet
+		printWriter.print("dia=\""+diameter+".\" ");
+		printWriter.println("len=\""+length+".\""); // case length? motor_length
+		printWriter.print("initWt=\""+mass+"\" "); // mass at i?
+		printWriter.print("propWt=\""+(mass - caseMass)+"\" ");
+		printWriter.println("delays=\""+1000+"\" auto-calc-mass=\"1\"");
+		printWriter.print("auto-calc-cg=\""+results.get(0).getCg()+"\" ");
+		printWriter.print("avgThrust=\""+results.get(0).getThrust()+"\" "); //mean
+		printWriter.print("peakThrust=\""+results.get(0).getThrust()+"\" "); //max
+		printWriter.println("throatDia=\""+throatDiameter+"\"");
+		printWriter.print("exitDia=\""+exitDiameter+"\" ");
+		printWriter.print("Itot=\""+impulse+".\" "); // added null value
+		printWriter.print("burn-time=\""+time+"\" "); //+time(length(time))+ original code
+		printWriter.print("massFrac=\""+massFrac+"\" ");
+		printWriter.println("Isp=\""+isp+"\"");
+		printWriter.println("tDiv=\"20\" tStep=\"-1.\" tFix=\"1\" FDiv=\"20\" FStep=\"-1.\" FFix=\"1\" mDiv=\"10\""); // All hard coded values
+		printWriter.print("mStep=\"-1.\" mFix=\"1\" cgDiv=\"10\" cgStep=\"-1.\" cgFix=\"1\">"); 
 		printWriter.println("    <data>");
 
-		//%Need to remove the case mass to get the propellant mass
+		//Need to remove the case mass to get the propellant mass
 		mass = mass-caseMass;
 
-		//%Time dependant data
-		for (int i = 1; i < time;i++){ // length(time)
+		//Time dependant data
+		for (int i = 0; i < time;i++){ // length(time)
 		    printWriter.print("      <eng-data  ");
-		    printWriter.print("t="+results.get(i).getTime()+" ");
-		    printWriter.print("f="+results.get(i).getThrust()+" ");
-		    printWriter.print("m="+results.get(i).getSystemMass()+" ");
-		    //printWriter.println("cg="+cg(i)+"/>"); //?
+		    printWriter.print("t=\""+results.get(i).getTime()+"\" ");
+		    printWriter.print("f=\""+results.get(i).getThrust()+"\" ");
+		    printWriter.print("m=\""+results.get(i).getSystemMass()+"\" ");
+		    printWriter.println("cg=\""+results.get(i).getCg()+"\"/>"); //?
 		}
+		
+		
+
+		//File close and cleanup
+		printWriter.println("    </data>");
+		printWriter.println("  </engine>");
+		printWriter.println("</engine-list>");
+		printWriter.println("</engine-database>");
+		printWriter.close();
 		}
 catch (FileNotFoundException e) {
 			
 			e.printStackTrace();
 		}
+	} 
+	
+	public static void main(String [] args){
+		String teamName = "TestTeam";
+		List <SimulationResults> results = new ArrayList<SimulationResults>();
+		SimulationResults sim1 = new SimulationResults();
+		SimulationResults sim2 = new SimulationResults();
+		SimulationResults sim3 = new SimulationResults();
+		SimulationResults sim4 = new SimulationResults();
+		SimulationResults sim5 = new SimulationResults();
+		sim1.setTime(0);
+		sim1.setThrust(0);
+		sim1.setSystemMass(0);
+		sim1.setCg(0);
 
+		sim2.setTime(0);
+		sim2.setThrust(0);
+		sim2.setSystemMass(0);
+		sim2.setCg(0);
 
-		//%File close and cleanup
-		//printWriter.print(fid","    </data>\n  </engine>\n</engine-list>\n</engine-database>\n");
-	} // RSEGenerator Constructor
+		sim3.setTime(0);
+		sim3.setThrust(0);
+		sim3.setSystemMass(0);
+		sim3.setCg(0);
+		
+		sim4.setTime(0);
+		sim4.setThrust(0);
+		sim4.setSystemMass(0);
+		sim4.setCg(0);
+		
+		sim5.setTime(0);
+		sim5.setThrust(0);
+		sim5.setSystemMass(0);
+		sim5.setCg(0);
+		
+		results.add(sim1);
+		results.add(sim2);
+		results.add(sim3);
+		results.add(sim4);
+		results.add(sim5);
+		
+		Case c = new Case(0, 0, 0);
+		SimulationSummary classifier = new SimulationSummary(results);
+		Nozzle no = new Nozzle(0, 0, 0, 0, 0);
+		
+		RSEGenerator test = new RSEGenerator(teamName, results, c, classifier, no);
+	}
 
-} // class RSEGenerator
+} 
